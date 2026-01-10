c3ce0d94c8860c6e36732f93baa04202115988c6
BlvckBytes <blvckbytes@gmail.com>
2026-01-10 23:31:17 +0100

    pipes: improved choosing the location at which leftovers are dropped

--- src/main/java/com/sk89q/craftbook/mechanics/pipe/Pipes.java
+++ src/main/java/com/sk89q/craftbook/mechanics/pipe/Pipes.java
@@ -32,6 +32,12 @@ import java.util.*;
 
 public class Pipes extends AbstractCraftBookMechanic implements PipesApi {
 
+    private static final BlockFace[] DROP_ITEM_FACES = new BlockFace[] {
+      BlockFace.UP, BlockFace.DOWN,
+      BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST,
+      BlockFace.NORTH_EAST, BlockFace.NORTH_WEST, BlockFace.SOUTH_EAST, BlockFace.SOUTH_WEST
+    };
+
     private int currentTubeBlockCounter;
     private int currentPistonBlockCounter;
 
@@ -524,11 +530,21 @@ public class Pipes extends AbstractCraftBookMechanic implements PipesApi {
         itemsInPipe.clear();
 
         if (!leftovers.isEmpty()) {
+            var dropBlock = containerBlock;
+            var nextFaceIndex = 0;
+
+            // Try to drop the item at a block that is not going to make it shoot out due to collision
+            while (nextFaceIndex < DROP_ITEM_FACES.length && !dropBlock.isPassable())
+                dropBlock = containerBlock.getRelative(DROP_ITEM_FACES[nextFaceIndex++]);
+
+            var dropLocation = dropBlock.getLocation().add(.5, .5, .5);
+            var dropWorld = containerBlock.getWorld();
+
             for (ItemStack item : leftovers) {
                 if (!ItemUtil.isStackValid(item))
                     continue;
 
-                inputPistonBlock.getWorld().dropItemNaturally(inputPistonBlock.getLocation().add(0.5, 0.5, 0.5), item);
+                dropWorld.dropItemNaturally(dropLocation, item);
             }
         }
 
