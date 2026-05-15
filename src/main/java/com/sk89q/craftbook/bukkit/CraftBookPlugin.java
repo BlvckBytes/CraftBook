fb3305e4479551cffbd24ea50cf2e53cbe80c688
BlvckBytes <blvckbytes@gmail.com>
2026-05-16 01:40:24 +0200

    deleted the dispenserrecipes mechanic

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -16,7 +16,6 @@ import com.sk89q.craftbook.mechanics.Sponge;
 import com.sk89q.craftbook.mechanics.Teleporter;
 import com.sk89q.craftbook.mechanics.XPStorer;
 import com.sk89q.craftbook.mechanics.area.Area;
-import com.sk89q.craftbook.mechanics.dispenser.DispenserRecipes;
 import com.sk89q.craftbook.mechanics.drops.CustomDrops;
 import com.sk89q.craftbook.mechanics.drops.legacy.LegacyCustomDrops;
 import com.sk89q.craftbook.mechanics.headdrops.HeadDrops;
@@ -192,7 +191,6 @@ public class CraftBookPlugin extends JavaPlugin {
 
         availableMechanics.put("Variables", VariableManager.class);
         availableMechanics.put("CommandItems", CommandItems.class);
-        availableMechanics.put("DispenserRecipes", DispenserRecipes.class);
         availableMechanics.put("CustomDrops", CustomDrops.class);
         availableMechanics.put("LegacyCustomDrops", LegacyCustomDrops.class);
         availableMechanics.put("PaintingSwitcher", PaintingSwitch.class);
