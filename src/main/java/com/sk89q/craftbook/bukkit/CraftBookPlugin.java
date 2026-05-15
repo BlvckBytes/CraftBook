9402b76a89525033f67a80d765c5f01dd0a6f70f
BlvckBytes <blvckbytes@gmail.com>
2026-05-16 01:29:25 +0200

    deleted the boatwaterplaceonly mechanic

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -16,7 +16,6 @@ import com.sk89q.craftbook.mechanics.Sponge;
 import com.sk89q.craftbook.mechanics.Teleporter;
 import com.sk89q.craftbook.mechanics.XPStorer;
 import com.sk89q.craftbook.mechanics.area.Area;
-import com.sk89q.craftbook.mechanics.boat.WaterPlaceOnly;
 import com.sk89q.craftbook.mechanics.cauldron.ImprovedCauldron;
 import com.sk89q.craftbook.mechanics.cauldron.legacy.Cauldron;
 import com.sk89q.craftbook.mechanics.crafting.CustomCrafting;
@@ -240,7 +239,6 @@ public class CraftBookPlugin extends JavaPlugin {
         availableMechanics.put("MinecartNoCollide", NoCollide.class);
         availableMechanics.put("MinecartPlaceAnywhere", PlaceAnywhere.class);
         availableMechanics.put("MinecartTemporaryCart", TemporaryCart.class);
-        availableMechanics.put("BoatWaterPlaceOnly", WaterPlaceOnly.class);
     }
 
     /**
