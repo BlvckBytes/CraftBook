1b271f077f2cfa18c6ce579fc4b5faea32498efb
BlvckBytes <blvckbytes@gmail.com>
2026-05-16 00:54:33 +0200

    deleted the betterphysics mechanic

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -9,7 +9,6 @@ import com.sk89q.craftbook.bukkit.util.CraftBookBukkitUtil;
 import com.sk89q.craftbook.core.LanguageManager;
 import com.sk89q.craftbook.core.st.MechanicClock;
 import com.sk89q.craftbook.core.st.SelfTriggeringManager;
-import com.sk89q.craftbook.mechanics.BetterPhysics;
 import com.sk89q.craftbook.mechanics.BetterPistons;
 import com.sk89q.craftbook.mechanics.BetterPlants;
 import com.sk89q.craftbook.mechanics.BounceBlocks;
@@ -225,7 +224,6 @@ public class CraftBookPlugin extends JavaPlugin {
         availableMechanics.put("CustomDrops", CustomDrops.class);
         availableMechanics.put("LegacyCustomDrops", LegacyCustomDrops.class);
         availableMechanics.put("PaintingSwitcher", PaintingSwitch.class);
-        availableMechanics.put("BetterPhysics", BetterPhysics.class);
         availableMechanics.put("HeadDrops", HeadDrops.class);
         availableMechanics.put("Marquee", Marquee.class);
         availableMechanics.put("TreeLopper", TreeLopper.class);
