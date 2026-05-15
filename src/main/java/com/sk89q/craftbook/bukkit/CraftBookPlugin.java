488073c196249b6aff9cc624640553ee36c573f6
BlvckBytes <blvckbytes@gmail.com>
2026-05-16 01:58:04 +0200

    deleted the customdrops (and legacycustomdrops) mechanic

--- src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
+++ src/main/java/com/sk89q/craftbook/bukkit/CraftBookPlugin.java
@@ -16,8 +16,6 @@ import com.sk89q.craftbook.mechanics.Sponge;
 import com.sk89q.craftbook.mechanics.Teleporter;
 import com.sk89q.craftbook.mechanics.XPStorer;
 import com.sk89q.craftbook.mechanics.area.Area;
-import com.sk89q.craftbook.mechanics.drops.CustomDrops;
-import com.sk89q.craftbook.mechanics.drops.legacy.LegacyCustomDrops;
 import com.sk89q.craftbook.mechanics.ic.ICMechanic;
 import com.sk89q.craftbook.mechanics.ic.gates.world.items.crafter.RecipeCache;
 import com.sk89q.craftbook.mechanics.items.CommandItemDefinition;
@@ -163,8 +161,6 @@ public class CraftBookPlugin extends JavaPlugin {
 
         availableMechanics.put("Variables", VariableManager.class);
         availableMechanics.put("CommandItems", CommandItems.class);
-        availableMechanics.put("CustomDrops", CustomDrops.class);
-        availableMechanics.put("LegacyCustomDrops", LegacyCustomDrops.class);
         availableMechanics.put("PaintingSwitcher", PaintingSwitch.class);
         availableMechanics.put("XPStorer", XPStorer.class);
         availableMechanics.put("HiddenSwitch", HiddenSwitch.class);
